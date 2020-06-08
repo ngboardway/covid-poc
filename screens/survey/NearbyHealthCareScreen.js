7b259477add76999f655a0ca58ff9cae8f141722
Natalie Boardway <ngboardway@gmail.com>
2020-06-08 14:38:36 -0400

    Beginning of using maps

--- screens/survey/NearbyHealthCareScreen.js
+++ screens/survey/NearbyHealthCareScreen.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { getNearbyHealthcare } from '../../api/GoogleMapsSearch';
 import Geolocation from 'react-native-geolocation-service';
+import MapView from 'react-native-maps';
 
 const NearbyHealthCareScreen = () => {
   const [healthcare, setHealthcare] = useState([])
+  const [coords, setCoords] = useState({});
 
   useEffect(() => {
     let establishPermissions = async () => {
@@ -13,6 +15,8 @@ const NearbyHealthCareScreen = () => {
 
     establishPermissions().then(
       Geolocation.getCurrentPosition((pos) => {
+        setCoords(pos.coords);
+
         getNearbyHealthcare(pos.coords.latitude, pos.coords.longitude, (result) => {
           console.log(result);
         })
@@ -25,9 +29,29 @@ const NearbyHealthCareScreen = () => {
     })
   }, [])
 
+  const getInitialRegion = () => {
+    return {
+      region: {
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+    };
+  }
+
+  const onRegionChange = (region) => {
+    setRegion({ region });
+  }
+
+  const [region, setRegion] = useState(getInitialRegion());
+
   return (
     <View style={styles.container}>
-
+      <MapView
+        region={region}
+        onRegionChange={onRegionChange}
+      />
     </View>
   )
 }
